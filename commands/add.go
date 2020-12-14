92241e7470a21260354f5fb31f2afec91ead6a2a
zayaz <zayaz20022001@gmail.com>
2020-12-14 18:47:58 +0200

    feat(commands) print and add

---
+++ commands/add.go
@@ -0,0 +1,15 @@
+package commands
+
+import (
+	"strconv" // The built-in package `strconv` provides the number parsing.
+	""github.com/FictProger/architecture-lab-4/engine"
+)
+
+type addCommand struct {
+	arg1, arg2 int
+}
+
+func (add *addCommand) Execute(loop engine.Handler) {
+	res := add.arg1 + add.arg2
+	loop.Post(&printCommand{arg: strconv.Itoa(res)})
+}
--- commands/print.go
+++ commands/print.go
@@ -1,15 +1,15 @@
 package commands
 
 import (
-	"fmt"
-
+	"fmt" // Package fmt implements formatted I/O with functions analogous to C's
+	      // printf and scanf. The format 'verbs' are derived from C's but are simpler.
 	"github.com/FictProger/architecture-lab-4/engine"
 )
 
-type PrintCommand struct {
-	Arg string
+type printCommand struct {
+	arg string
 }
 
-func (p *PrintCommand) Execute(loop engine.Handler) {
-	fmt.Println(p.Arg)
+func (print *PrintCommand) Execute(loop engine.Handler) {
+	fmt.Println(print.arg)
 }
